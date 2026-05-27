88b9d91816f9688ffe8d2283e8dc92ba6877cbdc
delfirodriguez <drodriguezb@fi.uba.ar>
2026-05-27 14:00:43 -0300

    feat: agregar validaciones a reservas.py

--- backend/app/routes/reservas.py
+++ backend/app/routes/reservas.py
@@ -1,72 +1,137 @@
 from flask import Blueprint, jsonify, request
 from db_connection import get_connection
 from utils import generar_qr, enviar_qr
-
+from datetime import date, datetime
 
 reservas = Blueprint("reservas", __name__)
 
+CAPACIDAD_MAX = 20
+
 @reservas.route("/reservas", methods = ["GET"]) # admin
 def ver_reservas():
-    conn = get_connection() 
-    cursor = conn.cursor() 
+    
+      try:
+        conn = get_connection() 
+        cursor = conn.cursor() 
+    
+        cursor.execute("SELECT * FROM reservas")
+        resultado = cursor.fetchall()
+          
+        return jsonify(resultado), 200
+        
+    except Exception as e:
+        return jsonify({"error": f"Error al obtener reservas: {str(e)}"}, 500
+                       
+    finally:
+        cursor.close()
+        conn.close()
 
-    cursor.execute("SELECT * FROM Reservas")
-    resultado = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return jsonify(resultado), 200
 
 @reservas.route("/reservas", methods=["POST"]) #cliente
-def crear_reserva():  
-    conn = get_connection()
-    cursor = conn.cursor()
-    data = request.get_json()  
+def crear_reserva(): 
+    try:
+        
+        conn = get_connection()
+        cursor = conn.cursor()
+        data = request.get_json()  
+        
+        if data is None: 
+            return jsonify({"error" :"Ingrese todos los datos, por favor"}), 400
     
-    if data is None: 
-        return jsonify({"error" :"Ingrese todos los datos, por favor"}), 400
+        if "mail" not in data or "cant_personas" not in data or "dia" not in data or "horario" not in data: 
+            return jsonify({"error" : "Datos no correspondientes"}), 400
+        try:
+            fecha_reserva = datetime.strptime(data["dia"], "%Y-%m-%d").date()
+        except ValueError:
+            return jsonify({"error": "Formato de fecha invalido}), 400
+        
+        if fecha_reserva < date.today():
+            return jsonify({"error": "No se puede reservar para una fecha que ya paso"}), 400
+                            
+        if data["cant_personas"] <= 0:
+            return jsonify({"error": "La cantidad de personas debe ser mayor a 0"}), 400
 
-    if "mail" not in data or "cant_personas" not in data or "dia" not in data or "horario" not in data: 
-        return jsonify({"error" : "Datos no correspondientes"}), 400    
+        if data["cant_personas"] > CAPACIDAD_MAX:
+            return jsonify({"error": f"La cantidad de personas no puede superar {CAPACIDAD_MAX}"}), 400
+
+        id_reserva = cursor.lastrowid
     
-    id_reserva = cursor.lastrowid
-
-    cursor.execute(""" INSERT INTO reservas(cant_personas, dia, horario) VALUES (%s, %s, %s)""", (data["cant_personas"], data["dia"], data["horario"]))
+        cursor.execute(""" INSERT INTO reservas(cant_personas, dia, horario) VALUES (%s, %s, %s)""", (data["cant_personas"], data["dia"], data["horario"]))
+        
+        qr_bytes = generar_qr(id_reserva)
+        enviar_qr(data["mail"], qr_bytes)
+        
     
-    qr_bytes = generar_qr(id_reserva)
-    enviar_qr(data["mail"], qr_bytes)
+        conn.commit() 
+        cursor.execute("SELECT * FROM reservas WHERE id_reserva = %s", (id_reserva,))
+        reserva_creada = cursor.fetchone()
+        return jsonify(reserva_creada), 201
+                      
+        cursor.close()
+        conn.close()
     
-
-    conn.commit()  
-    cursor.close()
-    conn.close()
-
-    return jsonify({"mensaje": "Reserva creada correctamente"}), 201
+        return jsonify({"mensaje": "Reserva creada correctamente"}), 201
+                      
+    except Exception as e:
+        return jsonify({"error": f"Error al crear la reserva: {str(e)}"}, 500
+                       
+    finally:
+        cursor.close()
+        conn.close()
 
 @reservas.route("/reservas/<int:id_reserva>", methods=["PATCH"]) # admin
 def actualizar_reserva(id_reserva):  
-    conn = get_connection()
-    cursor = conn.cursor()
-    data = request.get_json() 
+    try:             
+        conn = get_connection()
+        cursor = conn.cursor()
+        data = request.get_json() 
+            
+        cursor.execute("SELECT * FROM reservas WHERE id_reserva = %s", (id_reserva,))
+        if cursor.fetchone() is None:
+             return jsonify({"error": f"No exite reserva con ese id {id_reserva}"}, 404
+        if data is None:
+            return jsonify({"error": "Ingrese todos los datos"}, 400
 
-    cursor.execute("""UPDATE Reservas SET mesa = %s WHERE id_reserva = %s """, (data["mesa"], id_reserva)) 
-    conn.commit()  
-
-    cursor.close()
-    conn.close()
-
-    return jsonify({"mensaje": "Reserva actualizada correctamente"}), 200
-
+        for campo in ["cant_personas", "horario", "dia" ,"mesa"]:
+            if campo not in data: 
+                return jsonify({"error": f"Falta campo requerido  {campo}"}), 400
+    
+        cursor.execute("""UPDATE Reservas SET mesa = %s WHERE id_reserva = %s """, (data["mesa"], id_reserva)) 
+        conn.commit()  
+        cursor.execute("SELECT * FROM reservas WHERE id_reserva = %s", (id_reserva,))
+        reserva_actualizada = cursor.fetchone()
+        return jsonify(reserva_actualizada), 200
+    
+    except Exception as e:
+        return jsonify({"error": f"Error al actualizar reserva: {str(e)}"}, 500
+                       
+    finally:
+        cursor.close()
+        conn.close()
+    
+       
 @reservas.route("/reservas/<int:id_reserva>", methods=["DELETE"]) # admin
 def eliminar_reserva(id_reserva):
-    conn = get_connection() 
-    cursor = conn.cursor() 
+    try:
+        conn = get_connection() 
+        cursor = conn.cursor() 
+        
+        cursor.execute("SELECT * FROM reservas WHERE id_reserva = %s", (id_reserva,))
+        reserva = cursor.fetchone()
+        
+        if reserva is None:
+             return jsonify({"error": f"No exite reserva con ese id {id_reserva}"}, 404
+        
+        cursor.execute("DELETE FROM Reservas WHERE id_reserva = %s", (id_reserva,))  
+        conn.commit()
 
-    cursor.execute("DELETE FROM Reservas WHERE id_reserva = %s", (id_reserva,))  
-    conn.commit()
+        return jsonify(reserva), 200
 
-    cursor.close()
-    conn.close()
+    except Exception as e:
+        return jsonify({"error": f"Error al eliminar reserva: {str(e)}"}, 500
+                       
+    finally:
+        cursor.close()
+        conn.close()
+   
     
-    return jsonify({"mensaje": "Reserva eliminada correctamente"}), 200 
