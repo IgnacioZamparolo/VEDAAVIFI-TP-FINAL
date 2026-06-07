3c56b79e70dfd22d2c0f206b11739b8a249112a9
IgnacioZamparolo <izamparolo@fi.uba.ar>
2026-06-07 01:20:21 -0300

    chore: escribir credenciales de la DB como variables de entorno

--- backend/app/db_connection.py
+++ backend/app/db_connection.py
@@ -2,14 +2,20 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
+from constants import( 
+DB_USER, 
+DB_PASSWORD, 
+DATABASE,
+)
+
 load_dotenv()
 
 
 DB_CONFIG = {
     "host": "localhost",
-    "user": "admin",
-    "password": "1234",
-    "database": "parrilla_argentina"
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DATABASE
 }
 
 def get_connection():
